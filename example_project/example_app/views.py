4c96637f28737f64838be51c1eefa91ab7bf78e2
leoyuk <yuk.jongwha@gmail.com>
2024-06-11 06:34:03 +0900

    basic models, views

--- example_project/example_app/views.py
+++ example_project/example_app/views.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.http import HttpResponse
 
+from .models import Author, Book
+from django.shortcuts import render
+
 def hello(request):
     return HttpResponse("Hello world!")
 
@@ -11,3 +14,29 @@ def hello(request):
 def hello_rest_api(request):
     data = {'message' : 'Hello, REST API!'}
     return Response(data)
+
+
+def home(request):
+    # 객체 생성 및 데이터 베이스에 저장
+    author = Author(name = "John Doe", age=30)
+    author.save()
+
+    book = Book(title="First Book", author=author)
+    book.save()
+
+    #데이터 베이스에서 객체 검색
+
+    books = Book.objects.all() # 모든 책 검색
+    book = Book.objects.get(title = 'First Book') # 특정 책 검색
+    first_book = Book.objects.first()
+
+    #객체 업데이트
+    book.title = "New Book Title"
+    book.save()
+
+    #객체 삭제
+    book.delete()
+
+    return render(request, "home.html")
+
+
