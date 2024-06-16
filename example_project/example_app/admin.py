e93044547877b192fad51a76ea668303c4158b4d
leoyuk <yuk.jongwha@gmail.com>
2024-06-17 06:49:21 +0900

    set admin

--- example_project/example_app/admin.py
+++ example_project/example_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Book, Author
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'published_date', 'price']
+    search_fields = ['title', 'author__name']
+    list_filter = ['author', 'published_date']
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
