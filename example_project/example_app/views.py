039f211ea8a96e52d25e788521ca37b2f4901426
leoyuk <yuk.jongwha@gmail.com>
2024-07-04 06:50:59 +0900

    filter and serach

--- example_project/example_app/views.py
+++ example_project/example_app/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import viewsets
 from django.http import HttpResponse
 from django.views import View
@@ -140,7 +142,32 @@ def print_book(request):
     return Response(serializer.data)
 
 
-
+@permission_classes([AllowAny])
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    pagination_class = PageNumberPagination
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title', 'author']
+    ordering_fields = ['published_date', 'price']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # 요청에서 필터랑 매개변수 가져오기
+        title = self.request.query_params.get('title', None)
+        author = self.request.query_params.get('author', None)
+
+        # 필터링 적용
+        if title :
+            queryset = queryset.filter(title__icontains=title)
+        if author :
+            queryset = queryset.filter(author__icontains=author)
+
+        # 정렬 적용
+        ordering = self.request.query_params.get('ordering', None)
+        if ordering in self.ordering_fields:
+            queryset = queryset.order_by(ordering)
+
+        return queryset
+    
